a56119b30a0cce4afa3267288d22018a95934824
Prince <princebxr02@gmail.com>
2023-01-25 12:28:59 +0530

    maa tujhe salam

---
+++ src/com/demo/Airthmatic.java
@@ -0,0 +1,31 @@
+package com.demo;
+
+public class Airthmatic {
+    public int add(int a , int b){
+        return a+b;
+
+    }
+    public int sub(int a , int b){
+        return a-b;
+
+    }
+    public int mul(int a , int b){
+        return a*b;
+    }
+
+    public int div(int a , int b){
+        return a/b;
+    }
+
+    public static void main(String[] args){
+        Airthmatic airobj = new Airthmatic();
+        System.out.println("Adding two number "+airobj.add(5,3));
+        System.out.println("substraction two number "+airobj.sub(5,3));
+        System.out.println("multiple two number "+airobj.mul(5,3));
+        System.out.println("division two number "+airobj.div(5,3));
+    }
+}
+
+
+
+
